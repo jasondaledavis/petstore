97037a5af602ee7da40517099fffd514b1cae33d
Alex Bezuska <abezuska@gmail.com>
2018-02-01 16:48:52 -0500

    add flats directory, update main.css

---
+++ flats/header.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html  lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="stylesheet" href="main.css">
+    <script src="https://use.fortawesome.com/a789a9d9.js"></script>
+</head>
+
+<body>
+<div id="page" class="site">
+	<header id="masthead" class="site-header">
+
+    <!-- top dark bar -->
+    <div class="top-nav">
+      <div class="top-nav-inner flex-row">
+        <nav>
+					<ul>
+	          <li><a class="" href="tel:18552707387" title="">855.270.7387</a></li>
+	          <li><a class="" href="#" title="">Contact Us</a></li>
+	          <li><a class="" href="#" title="">Partnerships</a></li>
+	          <li><a class="" href="#" title="">About Us</a></li>
+	          <li><a class="" href="#" title="">MyPets Login</a></li>
+					</ul>
+        </nav>
+        <a class="search" href="#" title=""><i class="fa fa-search"></i></a>
+      </div>
+    </div>
+
+    <!-- main header bar -->
+
+		<div class="main-nav bg-white">
+      <div class="main-nav-inner flex-row">
+
+				<div class="mobile-contact-buttons">
+					<a href="tel:18552707387"><i class="fa fa-phone"></i></a>
+					<a href="#"><i class="fa fa-home"></i></a>
+				</div>
+
+        <div class="site-branding">
+            <a class="logo" href="/" rel="home"><img src="images/logo.svg" alt="Pet first logo"></a>
+    		</div><!-- .site-branding -->
+
+        <div class="nav-side flex-row">
+      		<nav id="site-navigation" class="main-nvaigation">
+						<ul>
+							<li><a href="#">Pet Insurance</a></li>
+							<li><a href="#">Customer Support</a></li>
+							<li><a href="#">Resources</a></li>
+							<li><a href="#">Blog</a></li>
+						</ul>
+      		</nav><!-- #site-navigation -->
+          <a class="btn-lg btn-primary quote-btn-lg" style="display:none;" href="#"> Get a Free Quote</a>
+					<a class="btn-lg btn-primary quote-btn-md" style="display:none;" href="#"> Get Free Quote</a>
+					<a class="btn-sm btn-primary quote-btn-sm" style="display:none;" href="#"> Get Quote</a>
+        </div>
+      </div>
+    </div>
+	</header><!-- #masthead -->
+
+	<div id="content" class="site-content">
