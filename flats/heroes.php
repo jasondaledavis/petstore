7b7f14419a6d7f790373cfe6e0e6f74121423d4a
Alex Bezuska <abezuska@gmail.com>
2018-02-08 12:09:34 -0500

    update heroes.php and css

--- flats/heroes.php
+++ flats/heroes.php
@@ -1,8 +1,8 @@
 <?php include 'header.php' ?>
 
 
-<!-- Hero Component right image two cta -->
-<div class="bg-light-1">
+<h2>Hero Component right image with 2 ctas</h2>
+<div class="hero bg-light-1">
   <div class="flex-container nopad fullbleed-left">
     <div class="flex-row">
       <div class="flex-col-sm-6">
@@ -26,8 +26,107 @@
 
       </div>
     </div>
-  </div><!-- /.container -->
+  </div>
 </div>
+
+
+<h2>Hero Component right image right 3 ctas</h2>
+<div class="hero bg-light-1">
+  <div class="flex-container nopad fullbleed-left">
+    <div class="flex-row">
+      <div class="flex-col-sm-6">
+
+        <h2 class="bold">Pet Insurance</h2>
+        <p>Many pet health insurance plans look similar from a far, but PetFirst breaks down the nuances so you can see which pet insurance provider can give you and your dog or cat the best medical coverage.Many pet health insurance plans look similar from a far, but PetFirst breaks down the nuances so you can see which pet insurance provider can give you and your dog or cat the best medical coverage. Many pet health insurance plans look similar from a far, but PetFirst breaks down the nuances so you can see which pet insurance provider can give you and your dog or cat the best medical coverage.</p>
+        <a class="btn-lg btn-primary" href="#" title="Large Primary Button">Get a Free Quote</a>
+
+      </div>
+      <div class="flex-col-sm-6 fullbleed-image hero-image" style="background-image: url(temp-images/Hero-Image-Pet-Insurance.jpg);">
+
+        <div class="hero-cta">
+          <span class="med-circle-icon"><i class="fa fa-dog-face"></i></span>
+          <h4 class="bold"> Dog Insurance <i class="fa fa-arrow-right"></i></h4>
+        </div>
+
+        <div class="hero-cta">
+          <span class="med-circle-icon"><i class="fa fa-cat-face"></i></span>
+          <h4 class="bold"> Cat Insurance <i class="fa fa-arrow-right"></i></h4>
+        </div>
+
+        <div class="hero-cta">
+          <span class="med-circle-icon"><i class="fa fa-cat-face"></i></span>
+          <h4 class="bold"> Cat Insurance <i class="fa fa-arrow-right"></i></h4>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<h2>Hero Component right image</h2>
+<div class="hero bg-light-1">
+  <div class="flex-container nopad fullbleed-left">
+    <div class="flex-row">
+      <div class="flex-col-sm-6">
+
+        <h2 class="bold">Pet Insurance</h2>
+        <p>Many pet health insurance plans look similar from a far, but PetFirst breaks down the nuances so you can see which pet insurance provider can give you and your dog or cat the best medical coverage.Many pet health insurance plans look similar from a far, but PetFirst breaks down the nuances so you can see which pet insurance provider can give you and your dog or cat the best medical coverage. Many pet health insurance plans look similar from a far, but PetFirst breaks down the nuances so you can see which pet insurance provider can give you and your dog or cat the best medical coverage.</p>
+        <a class="btn-lg btn-primary" href="#" title="Large Primary Button">Get a Free Quote</a>
+
+      </div>
+      <div class="flex-col-sm-6 fullbleed-image hero-image" style="background-image: url(temp-images/Hero-Image-Pet-Insurance.jpg);">
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<h2>Hero Component right video not autoplay</h2>
+<div class="hero bg-light-1">
+  <div class="flex-container nopad fullbleed-left">
+    <div class="flex-row">
+      <div class="flex-col-sm-6">
+
+        <h2 class="bold">Pet Insurance</h2>
+        <p>Many pet health insurance plans look similar from a far, but PetFirst breaks down the nuances so you can see which pet insurance provider can give you and your dog or cat the best medical coverage.Many pet health insurance plans look similar from a far, but PetFirst breaks down the nuances so you can see which pet insurance provider can give you and your dog or cat the best medical coverage. Many pet health insurance plans look similar from a far, but PetFirst breaks down the nuances so you can see which pet insurance provider can give you and your dog or cat the best medical coverage.</p>
+        <a class="btn-lg btn-primary" href="#" title="Large Primary Button">Get a Free Quote</a>
+
+      </div>
+      <div class="flex-col-sm-6 fullbleed-image hero-image" style="background-image: url(temp-images/Hero-Image-Pet-Insurance.jpg);">
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+<h2>Hero Component right image with quote form</h2>
+
+<div class="hero bg-light-1">
+  <div class="flex-container nopad fullbleed-left">
+    <div class="flex-row">
+      <div class="flex-col-sm-6">
+
+        <h2 class="bold">Pet Insurance</h2>
+        <p>Many pet health insurance plans look similar from a far, but PetFirst breaks down the nuances so you can see which pet insurance provider can give you and your dog or cat the best medical coverage.Many pet health insurance plans look similar from a far, but PetFirst breaks down the nuances so you can see which pet insurance provider can give you and your dog or cat the best medical coverage. Many pet health insurance plans look similar from a far, but PetFirst breaks down the nuances so you can see which pet insurance provider can give you and your dog or cat the best medical coverage.</p>
+        <a class="btn-lg btn-primary" href="#" title="Large Primary Button">Get a Free Quote</a>
+
+      </div>
+      <div class="flex-col-sm-6 fullbleed-image hero-image" style="background-image: url(temp-images/Hero-Image-Pet-Insurance.jpg);">
+        <div>
+          <h2 class="text-light-1 bold">Find a plan that fits</h2>
+          <p class="small">What is your pets's name?</p>
+          <div class="vertical-align-top">
+            <input class="flat-right" type="text" placeholder="We bet it's cute!"><a class="btn-half-left btn-primary btn-dark">Get my free quote!</a>
+          </div>
+          <p>google rating goes here</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
 
 
 
