70c650c306538d5d02acf43e78f42791c00579cf
Alex Bezuska <abezuska@gmail.com>
2018-02-08 11:14:49 -0500

    add new flats and media

---
+++ flats/heroes.php
@@ -0,0 +1,38 @@
+<?php include 'header.php' ?>
+
+
+<!-- Hero Component right image two cta -->
+<div class="bg-light-1">
+  <div class="flex-container nopad fullbleed-left">
+    <div class="flex-row">
+      <div class="flex-col-sm-6">
+
+        <h2 class="bold">Pet Insurance</h2>
+        <p>Many pet health insurance plans look similar from a far, but PetFirst breaks down the nuances so you can see which pet insurance provider can give you and your dog or cat the best medical coverage.Many pet health insurance plans look similar from a far, but PetFirst breaks down the nuances so you can see which pet insurance provider can give you and your dog or cat the best medical coverage. Many pet health insurance plans look similar from a far, but PetFirst breaks down the nuances so you can see which pet insurance provider can give you and your dog or cat the best medical coverage.</p>
+        <a class="btn-lg btn-primary" href="#" title="Large Primary Button">Get a Free Quote</a>
+
+      </div>
+      <div class="flex-col-sm-6 fullbleed-image hero-image" style="background-image: url(temp-images/Hero-Image-Pet-Insurance.jpg);">
+
+        <div class="hero-cta">
+          <span class="med-circle-icon"><i class="fa fa-dog-face"></i></span>
+          <h4 class="bold"> Dog Insurance <i class="fa fa-arrow-right"></i></h4>
+        </div>
+
+        <div class="hero-cta">
+          <span class="med-circle-icon"><i class="fa fa-cat-face"></i></span>
+          <h4 class="bold"> Cat Insurance <i class="fa fa-arrow-right"></i></h4>
+        </div>
+
+      </div>
+    </div>
+  </div><!-- /.container -->
+</div>
+
+
+
+
+
+
+<?php include 'footer.php' ?>
+<?php include 'bottom-navigation.php' ?>
