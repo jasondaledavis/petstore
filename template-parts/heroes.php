b910371c8d4736116ac298f108f01b05885a0ee1
Jason Davis <jasondaledavis@gmail.com>
2018-02-15 15:56:15 -0500

    revised heroes
    revised heroes.php

--- template-parts/heroes.php
+++ template-parts/heroes.php
@@ -8,64 +8,55 @@
  */
 ?>
 
-<!-- header with two cta's -->
+<!-- 2 cta -->
+<?php if ( get_post_meta( $post->ID, 'hero_cta_link_1', true ) ) { 
+  echo '<div class="circle-cta">
+    <span class="med-circle-icon"><i class="fa fa-dog-face"></i></span>
+    <h4 class="bold"> Dog Insurance <i class="fa fa-arrow-right"></i></h4>
+  </div>
 
-<?php if ( get_post_meta( $page_id, 'hero_header_cta_dog_and_cat_insurance', true ) ) { ?>
+  <div class="circle-cta">
+    <span class="med-circle-icon"><i class="fa fa-cat-face"></i></span>
+    <h4 class="bold"> Cat Insurance <i class="fa fa-arrow-right"></i></h4>
+  </div>';
+} ?>
+<!-- 3 ctas -->
 
-      <div class="flex-col-sm-6 fullbleed-image hero-image" style="background-image: url(<?php echo get_template_directory_uri();?>/assets/img/Hero-Image-Pet-Insurance.jpg);"> 
+<?php elseif ( get_post_meta( $post->ID, 'hero_header_cta_dog_and_cat_insurance_u', true ) ) { 
+  echo '<div class="circle-cta">
+    <span class="med-circle-icon"><i class="fa fa-dog-face"></i></span>
+    <h4 class="bold"> Dog Insurance <i class="fa fa-arrow-right"></i></h4>
+  </div>
 
-        <div class="hero-cta">
-          <span class="med-circle-icon"><i class="fa fa-dog-face"></i></span>
-          <h4 class="bold"> Dog Insurance <i class="fa fa-arrow-right"></i></h4>
-        </div>
+  <div class="circle-cta">
+    <span class="med-circle-icon"><i class="fa fa-cat-face"></i></span>
+    <h4 class="bold"> Cat Insurance <i class="fa fa-arrow-right"></i></h4>
+  </div>
 
-        <div class="hero-cta">
-          <span class="med-circle-icon"><i class="fa fa-cat-face"></i></span>
-          <h4 class="bold"> Cat Insurance <i class="fa fa-arrow-right"></i></h4>
-        </div>
+  <div class="circle-cta">
+    <span class="med-circle-icon"><i class="fa fa-cat-face"></i></span>
+    <h4 class="bold"> Cat Insurance <i class="fa fa-arrow-right"></i></h4>
+  </div>';
+} ?>
 
-<!-- need new custom field for this one header with 3 cta's -->
-<?php } elseif ( get_post_meta( $page_id, 'hero_header_cta_dog_and_cat_insurance', true ) ) { ?>
+<!-- image -->
+<?php elseif ( get_post_meta( $post->ID, 'single_image_header_k', true ) ) { 
+  echo '<div class="flex-col-sm-6 nopad hero-image" style="background-image: url(temp-images/Hero-Image-Pet-Insurance.jpg);">';
+} ?>
 
-      <div class="flex-col-sm-6 fullbleed-image hero-image" style="background-image: url(<?php echo get_template_directory_uri();?>/assets/img/Hero-Image-Pet-Insurance.jpg);">
+<!-- video -->
+<?php elseif ( get_post_meta( $post->ID, 'video_hero', true ) ) { 
+  echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/kEa8DdKwfvE" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
+} ?>
 
-        <div class="hero-cta">
-          <span class="med-circle-icon"><i class="fa fa-dog-face"></i></span>
-          <h4 class="bold"> Dog Insurance <i class="fa fa-arrow-right"></i></h4>
-        </div>
-
-        <div class="hero-cta">
-          <span class="med-circle-icon"><i class="fa fa-cat-face"></i></span>
-          <h4 class="bold"> Cat Insurance <i class="fa fa-arrow-right"></i></h4>
-        </div>
-
-        <div class="hero-cta">
-          <span class="med-circle-icon"><i class="fa fa-cat-face"></i></span>
-          <h4 class="bold"> Cat Insurance <i class="fa fa-arrow-right"></i></h4>
-        </div>
-
-<!-- header with simple image -->
-<?php } elseif ( get_post_meta( $page_id, 'hero_header_image', true ) ) { ?>
-
-      <div class="flex-col-sm-6 fullbleed-image hero-image" style="background-image: url(<?php echo get_template_directory_uri();?>/assets/img/Hero-Image-Pet-Insurance.jpg);">
-
-<!-- header with video background -->
-<?php } elseif ( get_post_meta( $page_id, 'hero_header_video', true ) ) { ?>
-
-      <div class="flex-col-sm-6 fullbleed-image hero-image"><video src="<?php echo get_template_directory_uri();?>/assets/img/PetFirst-Homepage-Video.mp4" autoplay loop playsinline muted></video></div>
-
-<!-- header with get a quote cta -->
-<?php } elseif ( get_post_meta( $page_id, 'get_a_quote_cta', true ) ) { ?>
-
-      <div class="flex-col-sm-6 fullbleed-image hero-image" style="background-image: url(<?php echo get_template_directory_uri();?>/assets/img/Hero-Image-Pet-Insurance.jpg);">
-
-        <div> 
-          <h2 class="text-light-1 bold">Find a plan that fits</h2>
-          <p class="small">What is your pets's name?</p>
-          <div class="vertical-align-top">
-            <input class="flat-right" type="text" placeholder="We bet it's cute!"><a class="btn-half-left btn-primary btn-dark">Get my free quote!</a>
-          </div>
-          <p>google rating goes here</p>
-
-<?php }?>
-
+<!-- quote form -->
+<?php elseif ( get_post_meta( $post->ID, 'get_a_quote_cta', true ) ) { 
+  echo '<div>
+  <h2 class="text-light-1 bold">Find a plan that fits</h2>
+  <p class="small">What is your pets\'s name\'?</p>
+  <div class="vertical-align-top">
+    <input class="flat-right" type="text" placeholder="We bet it\'s cute!"><a class="btn-half-left btn-primary btn-dark">Get my free quote!</a>
+  </div>
+  <p>google rating goes here</p>
+</div>';
+} ?>
