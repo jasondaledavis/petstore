fc5b26fb07cb651115c071a43d873f9bc7ba4102
Jason Davis <jasondaledavis@gmail.com>
2018-02-15 16:26:43 -0500

    commented out for errors
    heroes.php

--- template-parts/heroes.php
+++ template-parts/heroes.php
@@ -9,54 +9,64 @@
 ?>
 
 <!-- 2 cta -->
-<?php if ( get_post_meta( $post->ID, 'hero_cta_link_1', true ) ) { 
-  echo '<div class="circle-cta">
-    <span class="med-circle-icon"><i class="fa fa-dog-face"></i></span>
-    <h4 class="bold"> Dog Insurance <i class="fa fa-arrow-right"></i></h4>
-  </div>
+<?php 
+// if ( get_post_meta( $post->ID, 'hero_cta_link_1', true ) ) { 
+//   echo '<div class="circle-cta">
+//     <span class="med-circle-icon"><i class="fa fa-dog-face"></i></span>
+//     <h4 class="bold"> Dog Insurance <i class="fa fa-arrow-right"></i></h4>
+//   </div>
 
-  <div class="circle-cta">
-    <span class="med-circle-icon"><i class="fa fa-cat-face"></i></span>
-    <h4 class="bold"> Cat Insurance <i class="fa fa-arrow-right"></i></h4>
-  </div>';
-} ?>
+//   <div class="circle-cta">
+//     <span class="med-circle-icon"><i class="fa fa-cat-face"></i></span>
+//     <h4 class="bold"> Cat Insurance <i class="fa fa-arrow-right"></i></h4>
+//   </div>';
+// } 
+?>
 <!-- 3 ctas -->
 
-<?php elseif ( get_post_meta( $post->ID, 'hero_header_cta_dog_and_cat_insurance_u', true ) ) { 
-  echo '<div class="circle-cta">
-    <span class="med-circle-icon"><i class="fa fa-dog-face"></i></span>
-    <h4 class="bold"> Dog Insurance <i class="fa fa-arrow-right"></i></h4>
-  </div>
+<?php 
+// elseif ( get_post_meta( $post->ID, 'hero_header_cta_dog_and_cat_insurance_u', true ) ) { 
+//   echo '<div class="circle-cta">
+//     <span class="med-circle-icon"><i class="fa fa-dog-face"></i></span>
+//     <h4 class="bold"> Dog Insurance <i class="fa fa-arrow-right"></i></h4>
+//   </div>
 
-  <div class="circle-cta">
-    <span class="med-circle-icon"><i class="fa fa-cat-face"></i></span>
-    <h4 class="bold"> Cat Insurance <i class="fa fa-arrow-right"></i></h4>
-  </div>
+//   <div class="circle-cta">
+//     <span class="med-circle-icon"><i class="fa fa-cat-face"></i></span>
+//     <h4 class="bold"> Cat Insurance <i class="fa fa-arrow-right"></i></h4>
+//   </div>
 
-  <div class="circle-cta">
-    <span class="med-circle-icon"><i class="fa fa-cat-face"></i></span>
-    <h4 class="bold"> Cat Insurance <i class="fa fa-arrow-right"></i></h4>
-  </div>';
-} ?>
+//   <div class="circle-cta">
+//     <span class="med-circle-icon"><i class="fa fa-cat-face"></i></span>
+//     <h4 class="bold"> Cat Insurance <i class="fa fa-arrow-right"></i></h4>
+//   </div>';
+// } 
+?>
 
 <!-- image -->
-<?php elseif ( get_post_meta( $post->ID, 'single_image_header_k', true ) ) { 
-  echo '<div class="flex-col-sm-6 nopad hero-image" style="background-image: url(temp-images/Hero-Image-Pet-Insurance.jpg);">';
-} ?>
+<?php 
+// elseif ( get_post_meta( $post->ID, 'single_image_header_k', true ) ) { 
+//   echo '<div class="flex-col-sm-6 nopad hero-image" style="background-image: url(temp-images/Hero-Image-Pet-Insurance.jpg);">';
+// } 
+?>
 
 <!-- video -->
-<?php elseif ( get_post_meta( $post->ID, 'video_hero', true ) ) { 
-  echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/kEa8DdKwfvE" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
-} ?>
+<?php 
+// elseif ( get_post_meta( $post->ID, 'video_hero', true ) ) { 
+//   echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/kEa8DdKwfvE" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
+// } 
+?>
 
 <!-- quote form -->
-<?php elseif ( get_post_meta( $post->ID, 'get_a_quote_cta', true ) ) { 
-  echo '<div>
-  <h2 class="text-light-1 bold">Find a plan that fits</h2>
-  <p class="small">What is your pets\'s name\'?</p>
-  <div class="vertical-align-top">
-    <input class="flat-right" type="text" placeholder="We bet it\'s cute!"><a class="btn-half-left btn-primary btn-dark">Get my free quote!</a>
-  </div>
-  <p>google rating goes here</p>
-</div>';
-} ?>
+<?php 
+// elseif ( get_post_meta( $post->ID, 'get_a_quote_cta', true ) ) { 
+//   echo '<div>
+//   <h2 class="text-light-1 bold">Find a plan that fits</h2>
+//   <p class="small">What is your pets\'s name\'?</p>
+//   <div class="vertical-align-top">
+//     <input class="flat-right" type="text" placeholder="We bet it\'s cute!"><a class="btn-half-left btn-primary btn-dark">Get my free quote!</a>
+//   </div>
+//   <p>google rating goes here</p>
+// </div>';
+// } 
+?>
